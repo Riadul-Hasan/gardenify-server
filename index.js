11409c61c1988802e6bcd519014154211c6f7f46
Riadul-Hasan <riadb468@gmail.com>
2025-05-20 16:02:26 +0600

    server running

--- index.js
+++ index.js
@@ -1,10 +1,26 @@
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config()
+
 const app = express()
 const port = process.env.PORT || 3000;
 
 app.use(cors())
 app.use(express.json())
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 app.get("/", (req, res)=>{
     res.send("gardening server working")

a4b3ddb8e3c97f987daf6db5582d08032dc0614e
Riadul-Hasan <riadb468@gmail.com>
2025-05-20 15:36:41 +0600

    server run and ignore node_modules

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const cors = require('cors');
+const app = express()
+const port = process.env.PORT || 3000;
+
+app.use(cors())
+app.use(express.json())
+
+app.get("/", (req, res)=>{
+    res.send("gardening server working")
+})
+
+
+app.listen(port, ()=>{
+    console.groupCollapsed("Server is running successfully", port)
+})

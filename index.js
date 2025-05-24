a92082340dd7e0113ccb9b3e25565a8f5103e0fd
Riadul-Hasan <riadb468@gmail.com>
2025-05-24 21:22:14 +0600

    Final Project

--- index.js
+++ index.js
@@ -2,13 +2,10 @@ const express = require('express');
 const cors = require('cors');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
-
 const app = express()
 const port = process.env.PORT || 3000;
-
 app.use(cors())
 app.use(express.json())
-
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.nttz9vg.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
@@ -20,8 +17,6 @@ const client = new MongoClient(uri, {
     deprecationErrors: true,
   }
 });
-
-
 
 
 async function run() {
@@ -110,7 +105,6 @@ async function run() {
         res.send(result)
     })
   
-
     // users releted api
     app.post("/users", async(req, res)=>{
         const userProfile = req.body;
@@ -120,24 +114,13 @@ async function run() {
 
     // Send a ping to confirm a successful connection
     // await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log("Pinged your deployment. Successfully connected to MongoDB!");
   } finally {
-    // Ensures that the client will close when you finish/error
+    
     // await client.close();
   }
 }
 run().catch(console.dir);
-
-
-
-
-
-
-
-
-
-
-
 
 app.get("/", (req, res)=>{
     res.send("gardening server working")
